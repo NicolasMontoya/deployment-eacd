7144d0f737a046931d1ba85cd24b7de914087432
Nicolas <edward.nmontoya@gmail.com>
2021-03-16 23:02:50 -0500

    Updated models

--- app/models/schema.py
+++ app/models/schema.py
@@ -1,10 +1,20 @@
 '''Schema of all data received and sent back to the user'''
+from enum import Enum
 from pydantic import BaseModel, Field
 from datetime import datetime, time, timedelta
 from typing import Any
 
 from pydantic.types import PositiveInt
  
+class ModelType(str, Enum):
+     RF = 'RandomForest'
+     LR = 'LinearRegression'
+     CT = 'CustomPipeline'
+     BS = 'Baseline'
+
+class EvalMetric(str, Enum):
+     Q2 = 'Square'
+     CT = 'CustomMetric'
 
 class User(BaseModel):
     '''Base User schema'''
@@ -21,10 +31,8 @@ class Dataset(BaseModel):
     '''
     Schema of dataset
     '''
-    name: str = Field(None, title='The name of the todo', max_length=20)
-    is_completed: bool = Field(False, title='Determines if the todo is completed or not defaults to False')
-    dataset_type: str = Field(None, title='Dataset type')
-    url: str = Field(None, title='Dataset location')
+    name: str = Field(..., title='The name of the todo', max_length=20)
+    url: str = Field(..., title='Dataset location')
 
 class DatasetOutput(Dataset):
     '''
@@ -38,30 +46,20 @@ class Model(BaseModel):
     '''
     Schema of the definition model
     '''
-    name: str = Field(None, title="Name of the model", max_length=14, min_length=6)
-    type_model: str = Field(None, title="Model type")
-    version: PositiveInt = Field(None, title="Version of the model")
-    hiper_params: Any = Field(None, title="Hiper parameters model")
-    library: str = Field(None, title="Library model")
-    model_library_name: str = Field( None, title="Library model")
-    url: str = Field(None, title="Ubication url")
-    state: str = Field(None, title="State of the model"),
+    name: str = Field(..., title="Name of the model", max_length=14, min_length=6)
+    type_model: str = Field(..., title="Model type")
+    version: int = Field(..., title="Version of the model")
+    hiper_params: Any = Field(..., title="Hiper parameters model")
+    eval_metric: EvalMetric = Field(..., title="Default metric")
+    grid_search: bool = Field(False, title="Search best parameters")
+    url: str = Field(..., title="Ubication url")
     dataset: str = Field(None, title="dataset")
 class ModelOutput(Model):
     '''
     Schema of model
     '''
     id: str = Field(None, title='Unique identifier')
+    state: str = Field(None, title="State of the model")
     created_date: Any = Field(None, title="Creation date")
     last_updated_date: Any = Field(None, title="Modification date")
-class Prediction(BaseModel):
-    '''
-    Schema of data returned when a new user is created. Contains name, email, and id
-    '''
-    y_pred: Any = Field(None, title='Response of the model')
 
-class BikeData(Prediction):
-    '''
-    BikeData
-    '''
-    date: datetime = Field(None, title='Date of the prediction')
