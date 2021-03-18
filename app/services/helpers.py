afb3b284cba0cc57ef02d5c07d5f624cf8aa2383
Nicolas <edward.nmontoya@gmail.com>
2021-03-17 20:31:26 -0500

    Updated feature extractor

--- app/services/helpers.py
+++ app/services/helpers.py
@@ -25,10 +25,20 @@ class FeatureEngineering(BaseEstimator, TransformerMixin):
     )
     self.new_features['past_y'] = values
   def rolling_mean(self):
-    self.new_features['SMA_cnt_3'] = self.new_features.cnt.rolling(3,center=True).mean()
-    self.new_features['SMA_cnt_4'] = self.new_features.cnt.rolling(4,center=True).mean()
+    self.new_features['SMA_cnt_30'] = self.new_features.cnt.rolling(30,center=True,min_periods=1).mean()
+    self.new_features['SMA_cnt_60'] = self.new_features.cnt.rolling(60,center=True,min_periods=1).mean()
+  def rolling_median(self):
+    self.new_features['median_cnt_30'] = self.new_features.cnt.rolling(10,center=True,min_periods=1).median()
+    self.new_features['median_cnt_15'] = self.new_features.cnt.rolling(20,center=True,min_periods=1).median()
+  def last_day(self):
+    second_day_start = self.new_features.index[0] + timedelta(hours=23)
+    last_day_start = self.new_features.index[-1] - timedelta(days=1)
+    values = np.concatenate(
+        (self.new_features.loc[:second_day_start,'cnt'].values,
+        self.new_features.loc[:last_day_start,'cnt'].values)
+    )
+    self.new_features['last_day_same_hour'] = values
   def transform(self, X):
     self.new_features = X.copy()
     self.extract_past_y()
-    self.rolling_mean()
     return self.new_features
